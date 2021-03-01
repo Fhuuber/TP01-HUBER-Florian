8427b48cd45e10462a3010e2ac8814f0e8b63267
Fhuuber <43069168+Fhuuber@users.noreply.github.com>
2021-03-01 16:44:37 +0100

    TP01

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<?php
+<!doctype html>
+<html lang="fr">
 
-echo "Hello world!";
+<head>
+    <meta charset="utf-8">
+    <title>Formulaire d'inscription</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
+    <script src="script.js"></script>
+</head>
 
-?>
+<body>
+    <form id="form">
+        <input type="text" class="field" name="lastname" placeholder="Nom" /><br>
+        <input type="text" class="field" name="firstname" placeholder="Prénom" /><br>
+        <select name="civility" class="field selectField">
+            <option value="M">M</option>
+            <option value="Mme">Mme</option>
+            <option value="Autre">Autre</option>
+        </select><br>
+        <input type="text" class="field" name="address" placeholder="Adresse" /><br>
+        <input type="text" class="field" name="postalCode" placeholder="Code postal" /><br>
+        <input type="text" class="field" name="city" placeholder="Ville" /><br>
+        <input type="tel" class="field" name="phoneNumber" placeholder="Numéro de téléphone" /><br>
+        <input type="text" class="field" name="email" placeholder="Adresse mail" /><br>
+        <input type="text" class="field" name="login" placeholder="Login" /><br>
+        <input type="password" class="field" name="password" id="password" placeholder="Mot de passe" /><br>
+        <input type="password" class="field" name="confirmPassword" id="confirmPassword" placeholder="Confirmation du mot de passe" /><br>
+        <input type="submit" id="submitButton" value="Valider">
+    </form>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $("#form").validate({
+        errorClass: "fieldError",
+
+        rules: {
+
+            lastname: "required",
+            firstname: "required",
+            tel: {
+                required: true,
+                number: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            password: "required",
+            confirmPassword: "required",
+        },
+
+        messages: {
+            lastname: "Veuillez insérer votre nom",
+            firstname: "Veuillez insérer votre prénom",
+            phoneNumber: 'Veuillez insérer un numéro de téléphone',
+            email: "Veuillez saisir une adresse mail valide",
+            password: "Veuillez saisir un mot de passe",
+            confirmPassword: "Veuillez confirmer le mot de passe",
+        },
+
+        submitHandler: function (form) {
+            if ($("#form").valid())
+            {
+                form.submit();
+            }
+        }
+    });
+})
+
+$(document).ready(function () {
+    $("form").submit(function () {
+        if (!verifyPassword()) {
+            return false;
+        }
+    });
+})
+
+function verifyPassword() {
+    if ($("#password").val() != $("#confirmPassword").val()) {
+        alert("Les mots de passe doivent être identiques !");
+        return false;
+    }
+}
